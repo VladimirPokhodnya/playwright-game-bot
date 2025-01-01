851389f166a43920aa8b9cff963b90bf52450da2
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2025-01-01 17:00:37 +0700

    first commit

---
+++ src/main/java/com/github/vladimirpokhodnya/playwrightgameboot/PlaywrightGameBootApplication.java
@@ -0,0 +1,12 @@
+package com.github.vladimirpokhodnya.playwrightgameboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywrightGameBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaywrightGameBootApplication.class, args);
+    }
+}
---
+++ src/main/java/com/github/vladimirpokhodnya/playwrightgameboot/config/GameProperties.java
@@ -0,0 +1,17 @@
+package com.github.vladimirpokhodnya.playwrightgameboot.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "game")
+@Setter
+@Getter
+public class GameProperties {
+    String host;
+    String username;
+    String password;
+
+}
